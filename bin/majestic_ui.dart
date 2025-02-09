ef4ec2846e0524b5adf0ec056e29095274169c40
Ronak99 <punase.ronak99@gmail.com>
2025-02-09 20:13:41 +0530

    Added custom text for help

--- bin/majestic_ui.dart
+++ bin/majestic_ui.dart
@@ -1,39 +1,27 @@
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
-import 'package:majestic_ui/magestic_cli.dart';
+import 'package:majestic_ui/commands.dart';
+import 'package:majestic_ui/data/cli_content.dart';
 
-class AddCommand extends Command {
-  @override
-  final name = 'add';
-
-  @override
-  final description =
-      'Add beautiful, tested and reliable widgets to your Flutter project.';
-
-  AddCommand();
-
-  @override
-  Future<void> run() async {
-    if (argResults!.rest.isEmpty) {
-      print('Please specify a widget name');
-      return;
-    }
-
-    final widgetName = argResults!.rest.first;
-    final cli = MagesticCli();
-    await cli.add(widgetName);
+void main(List<String> arguments) async {
+  if (arguments.contains('--help') ||
+      arguments.contains('-h') ||
+      arguments.isEmpty ||
+      arguments.contains('help')) {
+    CliContent().printHelpText();
+    return;
   }
-}
 
-void main(List<String> arguments) {
   final runner = CommandRunner('magestic_cli',
       'A CLI tool for adding beautiful, tested and reliable widgets to your Flutter project.')
-    ..addCommand(AddCommand());
+    ..addCommand(AddCommand())
+    ..addCommand(DocsCommand());
+  // ..addCommand(TestCommand());
 
   runner.run(arguments).catchError((error) {
     if (error is! UsageException) throw error;
-    print(error);
+    CliContent().errorText(error.message);
     exit(64);
   });
 }
